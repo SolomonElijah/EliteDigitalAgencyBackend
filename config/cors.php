9fff20ebda0b1515705c8a350f1722a695c55623
SolomonElijah <solomonelijahsunday1@gmail.com>
2026-03-02 01:28:30 +0100

    i added cors

---
+++ config/cors.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['GET', 'POST', 'OPTIONS'],
+
+    'allowed_origins' => [
+        'https://elitedigitalagency.net',
+        'https://www.elitedigitalagency.net',
+        'https://portal.elitedigitalagency.net',
+        // Add any other frontend domains here
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['Content-Type', 'Accept', 'Authorization', 'X-Requested-With'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 86400,
+
+    'supports_credentials' => false,
+
+];
